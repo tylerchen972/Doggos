0fa14353af0f0b55cad2cb5c1e5a2b17bf180c92
steven-jiang-cs <stevenji@buffalo.edu>
2021-10-02 15:53:25 -0400

    Testing db connection #25
    Attempting to connect to postgres db

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-var express = require("express");
+// var express = require("express");
 
-var app = express();
-var host = "0.0.0.0";
-var port = "5000";
+// var app = express();
+// var host = "0.0.0.0";
+// var port = "5000";
 
-app.use(express.static("public"));
-if(process.argv.length > 2) {
-    port = process.argv[2];
-}
+// app.use(express.static("public"));
+// if(process.argv.length > 2) {
+//     port = process.argv[2];
+// }
 
-app.listen(port, host, () => {
-    console.log(host, port)
-});
+// app.listen(port, host, () => {
+//     console.log(host, port)
+// });
+
+// https://expressjs.com/en/guide/database-integration.html#postgresql
+
+var postgresSQL = require("pg-promise");
+var db = postgresSQL(process.env.DATABASE_URL); 
+
+db.one('CREATE TABLE test (col1     string, col2    string);');
+
+db.one('INSERT INTO test VALUES ("hello", "world");'); 
+
+// db.one('SELECT * FROM test;').then(function (data) {
+//     console.log('DATA: ', data.value).catch(function (error) {
+//         console.log('ERROR: ', error)
+//     })
+// });
+
 
