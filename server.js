757aaf86f31d44edbd4c23132d18963c43c3812e
steven-jiang-cs <stevenji@buffalo.edu>
2021-10-02 15:55:15 -0400

    Testing db connection again #25

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// var express = require("express");
+var express = require("express");
 
-// var app = express();
-// var host = "0.0.0.0";
-// var port = "5000";
+var app = express();
+var host = "0.0.0.0";
+var port = "5000";
 
-// app.use(express.static("public"));
-// if(process.argv.length > 2) {
-//     port = process.argv[2];
-// }
+app.use(express.static("public"));
+if(process.argv.length > 2) {
+    port = process.argv[2];
+}
 
-// app.listen(port, host, () => {
-//     console.log(host, port)
-// });
+app.listen(port, host, () => {
+    console.log(host, port)
+});
 
 // https://expressjs.com/en/guide/database-integration.html#postgresql
 
