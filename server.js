221817828f6254659b2e6267bbf7b3bea721c728
MustafaRa46 <mmraja@buffalo.edu>
2021-10-04 18:41:15 -0400

    Fixing errors #25

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ const e = require('express');
   database: 'de74re8hchfnir',
   password: '90666c2149bf70d1f3581fac87b3e359dd3f3363f5b157a1835a63dce33356bd',
   port: 5432,
-  ssl: {
-    rejectUnauthorized: false
-  },
 })
 
 var app = expressApp();
