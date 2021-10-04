eef32476d5310d99c9cd006cac40c2ead3c2bc81
MustafaRa46 <mmraja@buffalo.edu>
2021-10-04 18:33:58 -0400

    Fixing errors #25

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ const e = require('express');
 
   user: 'hqphzezcxezigz',
   host: 'ec2-54-227-246-76.compute-1.amazonaws.com',
-  ssl: {
-    rejectUnauthorized: false
-  },
   database: 'de74re8hchfnir',
   password: '90666c2149bf70d1f3581fac87b3e359dd3f3363f5b157a1835a63dce33356bd',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  },
 })
 
 var app = expressApp();
