7295dd81fb156eef9b33da0ca1aac44b3e9feb1f
MustafaRa46 <mmraja@buffalo.edu>
2021-10-04 17:20:49 -0400

    Changing database to heroku credentials #25

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ const e = require('express');
 const Pool = require('pg').Pool;
 const { Console } = require('console');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'doggos_master',
-  password: 'root',
+  user: 'hqphzezcxezigz',
+  host: 'ec2-54-227-246-76.compute-1.amazonaws.com',
+  database: de74re8hchfnir',
+  password: '90666c2149bf70d1f3581fac87b3e359dd3f3363f5b157a1835a63dce33356bd',
   port: 5432,
 })
 
