0f36498905b645a101a77870ba620f1a73d22f9e
tylerchen972 <tylerchen972@gmail.com>
2021-10-18 20:00:25 -0400

    updated database security

--- dbconfig.js
+++ dbconfig.js
@@ -2,21 +2,14 @@ const Pool = require('pg').Pool;
 const { Console } = require('console');
 const { query } = require('express');
 const pool = new Pool({
-  user: 'hqphzezcxezigz',
-  host: 'ec2-54-227-246-76.compute-1.amazonaws.com',
-  database: 'de74re8hchfnir',
-  password: '90666c2149bf70d1f3581fac87b3e359dd3f3363f5b157a1835a63dce33356bd',
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
   port: 5432,
   sslmode:true,
   ssl: {rejectUnauthorized: false},
 })
 
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'doggos_mas',
-//     password: 'abc123',
-//     port: 5432,
-// })
 module.exports = pool;
